dbe374c4ae342ffedd71436ac74a2c8d44cb1aee
isra-osvaldo <israel.acuna7@gmail.com>
2025-06-02 17:45:59 -0400

    refactor CardPizza component to improve readability and structure

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,29 +1,32 @@
 import { formatNumberCL } from "../helpers/formatNumber"
 
-
 const CardPizza = ({ name, price, ingredients, img }) => {
-  return (
-    <article className="card">
-        <img 
-            src= { img } 
-            alt= { name } />
-            <div className="card-body">
-                <div className="title">
-                    <h4>Pizza { name } </h4>
-                </div>
-                <div className="ingredients-box">
-                    <p className="ingredients-title">Ingredientes:</p>
-                    <p className="ingredients">🍕 { ingredients.join(", ") }</p>
-                </div>  
-                <div className="buy">
-                    <h4>Precio: ${ formatNumberCL(price) }</h4>
-                    <div className="buttons-buy">
-                        <button className="btn btn-ver-mas">Ver más 👀</button>
-                        <button className="btn btn-añadir">Añadir 🛒</button>
+
+    const priceFormat = formatNumberCL(price)
+    const ingredientsToString = ingredients.join(", ")
+
+    return (
+        <article className="card">
+            <img 
+                src= { img } 
+                alt= { name } />
+                <div className="card-body">
+                    <div className="title">
+                        <h4>Pizza { name } </h4>
+                    </div>
+                    <div className="ingredients-box">
+                        <p className="ingredients-title">Ingredientes:</p>
+                        <p className="ingredients">🍕 { ingredientsToString }</p>
+                    </div>  
+                    <div className="buy">
+                        <h4>Precio: ${ priceFormat }</h4>
+                        <div className="buttons-buy">
+                            <button className="btn btn-ver-mas">Ver más 👀</button>
+                            <button className="btn btn-añadir">Añadir 🛒</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-    </article>
+        </article>
   )
 }
 
