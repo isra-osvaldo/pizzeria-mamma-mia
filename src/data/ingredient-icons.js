5e00bcb2e55733f61663a91579f8c64d8900dec3
isra-osvaldo <israel.acuna7@gmail.com>
2025-06-15 20:10:00 -0400

    add ingredient icons and capitalizeWords helper function

---
+++ src/data/ingredient-icons.js
@@ -0,0 +1,13 @@
+export const icons = {
+  mozzarella: '🧀',
+  tomates: '🍅',
+  jamón: '🍖',
+  orégano: '🌿',
+  choricillo: '🥓',
+  aceitunas: '🫒',
+  champiñones: '🍄',
+  bacon: '🥓',
+  tomates_cherry: '🍅',
+  pimientos: '🌶️',
+  pollo_grillé: '🍗',
+}
---
+++ src/helpers/capitalizeWords.js
@@ -0,0 +1,9 @@
+const capitalizeWords = (str) => {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+export { capitalizeWords }
